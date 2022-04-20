4bde9f0714d89d92d35fcc2f728b8c9538ed38a3
Hoàng Long <95578454+ManjiroSano29@users.noreply.github.com>
2022-04-20 08:32:06 +0000

    h

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const Article = require('../models/Article');
 const Comment = require('../models/Comment');
-var passportLocalMongoose=require("passport-local-mongoose");
 const userSchema = new Schema({
   name: {
     type: String,
@@ -26,18 +25,6 @@ const userSchema = new Schema({
   resetPasswordToken: String,
   resetPasswordExpires: Date
 });
-var options = {
-    errorMessages: {
-        MissingPasswordError: 'No password was given',
-        AttemptTooSoonError: 'Account is currently locked. Try again later',
-        TooManyAttemptsError: 'Account locked due to too many failed login attempts',
-        NoSaltValueStoredError: 'Authentication not possible. No salt value stored',
-        IncorrectPasswordError: 'Password or username are incorrect',
-        IncorrectUsernameError: 'Password or username are incorrect',
-        MissingUsernameError: 'No username was given',
-        UserExistsError: 'A user with the given username is already registered'
-    }
-};
-userSchema.plugin(passportLocalMongoose, options);
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
